b06b84cbcf0c6f68b7cfc3935d2ebb34e00b4f1d
Ehab <ehab.alyounes@gmail.com>
2020-09-29 00:36:56 +0300

    Added Plot1

---
+++ plot1.R
@@ -0,0 +1,19 @@
+library(data.table)
+
+# Reading data
+data <- fread("household_power_consumption.txt", na.strings = "?",
+              colClasses = c("character", "character", rep("numeric", 7)))
+
+# Filter it to only the 1st and 2nd of Feb 2007
+data <- subset(data, Date == "1/2/2007" | Date == "2/2/2007")
+
+# Combine Date and Time to form a single DateTime
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S")
+
+
+png("plot1.png", width = 480, height = 480)
+
+hist(data$Global_active_power, xlab = "Global Active Power (kilowatts)",
+     main = "Global Active Power", col = "#ff2500")
+
+dev.off()
