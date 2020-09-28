b4b1b3823515fa800961fca19c4a83eb5528a684
Ehab <ehab.alyounes@gmail.com>
2020-09-29 00:41:39 +0300

    fixed code style

--- plot1.R
+++ plot1.R
@@ -1,5 +1,9 @@
 library(data.table)
 
+################################################################################
+#                               Data Processing                                #
+################################################################################
+
 # Reading data
 data <- fread("household_power_consumption.txt", na.strings = "?",
               colClasses = c("character", "character", rep("numeric", 7)))
@@ -10,6 +14,9 @@ data <- subset(data, Date == "1/2/2007" | Date == "2/2/2007")
 # Combine Date and Time to form a single DateTime
 data$DateTime <- as.POSIXct(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S")
 
+################################################################################
+#                                   Plotting                                   #
+################################################################################
 
 png("plot1.png", width = 480, height = 480)
 
