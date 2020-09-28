1036a7e6c0383561a191d332fd14e362d165b160
Ehab <ehab.alyounes@gmail.com>
2020-09-29 01:07:46 +0300

    Created a constant instead of magical values

--- plot1.R
+++ plot1.R
@@ -11,16 +11,15 @@ data <- fread("household_power_consumption.txt", na.strings = "?",
 # Filter it to only the 1st and 2nd of Feb 2007
 data <- subset(data, Date == "1/2/2007" | Date == "2/2/2007")
 
-# Combine Date and Time to form a single DateTime
-data$DateTime <- as.POSIXct(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S")
-
 ################################################################################
 #                                   Plotting                                   #
 ################################################################################
 
+RED <- "#ff2500"
+
 png("plot1.png", width = 480, height = 480) # open file
 
 hist(data$Global_active_power, xlab = "Global Active Power (kilowatts)",
-     main = "Global Active Power", col = "#ff2500") # plot
+     main = "Global Active Power", col = RED) # plot
 
 dev.off() # close file
